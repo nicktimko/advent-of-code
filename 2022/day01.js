15b0375cd99153e8e4a6cdbacf44c0fa87e3d493
Nick Timkovich <prometheus235@gmail.com>
2022-12-03 23:33:29 -0600

    Refactor a bit for day 2

--- 2022/day01.js
+++ 2022/day01.js
@@ -1,16 +1,29 @@
-function readInputFile(e) {
-    var file = e.target.files[0];
-    if (!file) {
-        return;
+/*
+Day 01...and more...
+*/
+
+function readInputFileFactory(callback) {
+    return function (e) {
+        var file = e.target.files[0];
+        if (!file) {
+            return;
+        }
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            var contents = e.target.result;
+            callback(contents);
+        };
+        reader.readAsText(file);
     }
-    var reader = new FileReader();
-    reader.onload = function(e) {
-        var contents = e.target.result;
-        solveDay01(contents);
-    };
-    reader.readAsText(file);
 }
 
+function addItem(olul, txt) {
+    var li = document.createElement('li');
+    txt = document.createTextNode(txt);
+    li.appendChild(txt);
+    olul.appendChild(li);
+    return li;
+}
 
 function solveDay01(input) {
     var outputFirst = document.getElementById("day01-output-1");
@@ -28,17 +41,13 @@ function solveDay01(input) {
             elf.push(Number.parseInt(line));
         }
     }
-    // console.log(elves);
 
-    var li = document.createElement('li');
-    txt = document.createTextNode('There are ' + elves.length.toString() + ' elves.');
-    li.appendChild(txt);
-    infoElement.appendChild(li);
+    addItem(infoElement, 'There are ' + elves.length.toString() + ' elves.');
 
     var maxElf = 0;
     var elfTotals = new Array();
     for (elf of elves) {
-        thisElf = elf.reduce((a,b)=>a+b, 0);
+        thisElf = elf.reduce((a, b) => a + b, 0);
         elfTotals.push(thisElf);
 
         maxElf = Math.max(thisElf, maxElf);
@@ -46,8 +55,20 @@ function solveDay01(input) {
     outputFirst.textContent = maxElf;
 
     // second half...
-    elfTotals.sort((a, b) => b - a);
+    elfTotals.sort((a, b) => b - a);  // reverse sort
     outputSecond.textContent = (elfTotals[0] + elfTotals[1] + elfTotals[2]).toString();
 }
 
-document.getElementById('day01-input').addEventListener('change', readInputFile, false);
+function solveDay02(input) {
+    var outputFirst = document.getElementById("day02-output-1");
+    var outputSecond = document.getElementById("day02-output-2");
+    var infoElement = document.getElementById("day02-info");
+
+    addItem(infoElement, "WIP.");
+}
+
+document.getElementById('day01-input')
+    .addEventListener('change', readInputFileFactory(solveDay01), false);
+
+document.getElementById('day02-input')
+    .addEventListener('change', readInputFileFactory(solveDay02), false);
