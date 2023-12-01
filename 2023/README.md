75ddace3d9102b5caa320520392c2594267a1ca7
Nicholas Timkovich <prometheus235@gmail.com>
2023-11-30 23:57:11 -0600

    23.01.2 wip

--- 2023/README.md
+++ 2023/README.md
@@ -8,3 +8,6 @@ I worked with Rust before, but didn't do much. Maybe more this year? I'm coming 
 
 Looking through [Rust by Example](https://doc.rust-lang.org/rust-by-example/index.html) it's [file I/O section](https://doc.rust-lang.org/rust-by-example/std_misc/file.html) is buried behind "std misc"? Strange. Was going to work on creating basic file I/O readers, but will maybe do that later.
 
+Strat to iterate over lines, and within the lines iterate from either side. Had vals `first|last_num` to store what I found, but the compiler was mad that they were maybe uninitalized because it didn't know that the loops would (should...given the input) set them. Just added 10x the first num found, and 1x the second.
+
+Could do the second part by replacing the words and reusing the same logic, or a regex for the words/digits and get the first/last match. Word replace sounds best?
--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -1,7 +1,15 @@
 use std::fs::read_to_string;
 
+// use regex::RegexBuilder;
+
+struct WordNums {
+    word: String,
+    num: u8,
+}
+
 fn main() {
     let mut accum = 0u32;
+    let mut accum2 = 0u32;
 
     for line in read_lines("inputs/day01.txt") {
         // let first_num: u32;
@@ -32,7 +40,66 @@ fn main() {
         // println!("{} -> {}{}", line, first_num, last_num);
     }
 
-    println!("{}", accum);
+    let wns: [WordNums; 9] = [
+        WordNums{word: "one".to_string(), num: 1},
+        WordNums{word: "two".to_string(), num: 2},
+        WordNums{word: "three".to_string(), num: 3},
+        WordNums{word: "four".to_string(), num: 4},
+        WordNums{word: "five".to_string(), num: 5},
+        WordNums{word: "six".to_string(), num: 6},
+        WordNums{word: "seven".to_string(), num: 7},
+        WordNums{word: "eight".to_string(), num: 8},
+        WordNums{word: "nine".to_string(), num: 9},
+    ];
+
+    for line in read_lines("inputs/day01.txt") {
+        for wn in wns {
+        }
+        for c in line.chars() {
+            let digit = c.to_digit(10);
+            match digit {
+                None => continue,
+                Some(val) => {
+                    // first_num = val;
+                    accum += 10*val;
+                    break
+                }
+            }
+        }
+
+        for c in line.chars().rev() {
+            let digit = c.to_digit(10);
+            match digit {
+                None => continue,
+                Some(val) => {
+                    // last_num = val;
+                    accum += val;
+                    break
+                }
+            }
+        }
+        // println!("{} -> {}{}", line, first_num, last_num);
+    }
+
+    // println!("Part 1: {}", accum);
+    // let pat = r"
+    //     (
+    //         [0-9]  # single digit
+    //         |one   # ...or 1-9 in english
+    //         |two
+    //         |three
+    //         |four
+    //         |five
+    //         |six
+    //         |seven
+    //         |eight
+    //         |nine
+    //     )
+    // ";
+    // let re = RegexBuilder::new(pat)
+    //     .ignore_whitespace(true)
+    //     .build()
+    //     .unwrap();
 
 }
 
